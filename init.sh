bae42753d3679c37299124208f29613983667b99
dchilot <dchilot@hotmail.com>
2016-10-27 21:24:03 +0000

    Fix formatting and comments. Add comments.

--- init.sh
+++ init.sh
@@ -1,3 +1,10 @@
+# Download the Packages file used to know about the different packages.
+# You may want to download a recent version, possibly by removing the
+# one you have.
+# This is mostly an example and fits some needs. A file for a different
+# debian distribution should work too.
+# This is needed by rapt-get.pp
+
 if [ ! -e Pacakges ] ; then
 	wget http://ftp.debian.org/debian/dists/jessie/main/binary-armel/Packages.xz
 	xz -d Packages.xz 
--- tarsync.sh
+++ tarsync.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env zsh
 
-# Copy a tar.gz generated with tartinator and extract.
+# Copy a tar.gz generated with tartinator and extract it.
 # This is a simple script with no safety.
 # $1: tarball
 # $2: user to connect with (ssh passwordless)
@@ -9,27 +9,27 @@
 
 main()
 {
-    set -e
+	set -e
 	tarball=$1
-    user=$2
-    machine=$3
-    folder=$4
-    if [ -z "$user" ] ; then
-        echo "User is missing, take root" >&2
-        user=root
-    fi
-    if [ -z "$folder" ] ; then
-        echo "Folder is missing, take \$HOME" >&2
-        folder=$(ssh $user@$machine 'echo $HOME')
-    fi
+	user=$2
+	machine=$3
+	folder=$4
+	if [ -z "$user" ] ; then
+		echo "User is missing, take root" >&2
+		user=root
+	fi
+	if [ -z "$folder" ] ; then
+		echo "Folder is missing, take \$HOME" >&2
+		folder=$(ssh $user@$machine 'echo $HOME')
+	fi
 	if [ ! -e "$tarball" ] ; then
 		echo "File '$tarball' does not exist" >&2
 		exit 1
 	fi
-    tar_name=$(basename $tarball)
-    echo scp $tarball $user@$machine:$folder/
-    scp $tarball $user@$machine:$folder
-    ssh $user@$machine "cd / ; tar xzf $folder/$tar_name"
+	tar_name=$(basename $tarball)
+	echo scp $tarball $user@$machine:$folder/
+	scp $tarball $user@$machine:$folder
+	ssh $user@$machine "cd / ; tar xzf $folder/$tar_name"
 }
 
 main $*
